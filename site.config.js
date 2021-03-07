299d8d450d80ba43ea9797707f2468c96e833814
Edgar Ferreira <contato@edgar-ferreira.com>
2021-03-07 12:18:17 -0300

    Seting up notion

--- site.config.js
+++ site.config.js
@@ -13,7 +13,7 @@ module.exports = {
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: 'Edgar Ferreira',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
