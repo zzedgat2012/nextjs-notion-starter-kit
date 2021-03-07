ea7bc5c1a7005de93cbdb2d02b661186db3e4db6
Edgar Ferreira <contato@edgar-ferreira.com>
2021-03-07 11:07:18 -0300

    Setup

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '58d6f58b3e4046feb99b98ca0a569e3f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: 'Edgar Ferreira',
   domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Edgar Ferreira',
 
   // open graph metadata (optional)
   description: 'Example site description',
