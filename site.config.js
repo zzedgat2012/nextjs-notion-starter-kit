37f16a213f895533b1ccfe81798b1bf0611d09ee
Edgar Ferreira <contato@edgar-ferreira.com>
2021-03-07 15:22:32 -0300

    notion setup

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '58d6f58b3e4046feb99b98ca0a569e3f',
+  rootNotionPageId: '21ed013e8fa5496a96d702ad2e481785',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
